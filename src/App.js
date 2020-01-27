57aeb7be2a2f62ba96603b535b011685561e8dd8
Bant89 <bant8009@gmail.com>
2020-01-27 17:02:56 -0400

    Added basic routes to the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Nav from './components/Nav';
+import Main from './pages/Main.js'
+import Login from './pages/Login.js'
+import MovieDetail from './pages/MovieDetail.js'
+import UserProfile from './pages/UserProfile.js'
 import './App.css'
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Nav />
-    </div>
+      <Switch>
+        <Route path="/" exact component={Main}/>
+        <Route path="/genre/:id" component={Main}/>
+        <Route path="/user/:id" component={UserProfile}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/movie/:id" component={MovieDetail}/>
+      </Switch>
+    </Router>
   );
 }
 
