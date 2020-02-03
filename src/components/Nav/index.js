b04c81d6b434341e2c0a0a6532ef182a55853537
Bant89 <bant8009@gmail.com>
2020-02-03 07:25:24 -0400

    Improved layout of the nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { change_genre, get_data } from '../../actions'
 import { genres } from '../../api/info'
+import { NavBar, NavItem, TopDiv } from './styles'
 
 const Nav = () => {
   const genre = useSelector(state => state.genre)
   const dispatch = useDispatch();
   return (
-    <ul>
-      <li>Watch Hot</li>
+    <NavBar>
+      <NavItem>
+        <TopDiv>
+          Watch Hot 
+          <ion-icon name="close"></ion-icon>
+        </TopDiv>
+      </NavItem>
       {genres.map(gen => {
-        return <li key={gen.id} onClick={() => {
+        return <NavItem key={gen.id} onClick={() => {
           dispatch(change_genre({id: gen.id, name: gen.name}))
           dispatch(get_data(gen))
-        }}>{gen.name}</li>
+        }}>{gen.name}</NavItem>
       })}
-    </ul>
+    </NavBar>
   )
 }
 
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -1,2 +1,36 @@
+import styled, { css } from 'styled-components'
 
+export const TopDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+export const NavBar = styled.ul`
+  list-style: none;
+  font-size: 1.3em;
+  padding: 0;
+  width: 70vw;
+  position: absolute;
+  background-color: white;
+  top: 0;
+  margin: 0;
+  left: 0;
+  z-index: 10;
+`;
+
+export const NavItem = styled.li`
+  margin: 0.3em 0;
+  padding-left: 0.7em;
+  cursor: pointer;
+  transition: all .5s ease-in-out;
+  ${props => props.chosen && css`
+    color: red;
+    font-weight: 800;
+  `}
+  &:hover {
+    background-color: #ff5252;
+    color: white;
+    transform-origin: left;
+  }
+`;
