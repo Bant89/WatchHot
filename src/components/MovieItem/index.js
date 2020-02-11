d1cca175a7296b8c58be8477c238606d170f3465
Bant89 <bant8009@gmail.com>
2020-02-11 12:47:06 -0400

    Added more details to the main results movies

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,20 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Item, Image } from './styles'
+import { TagList } from '../TagList'
+import { getStars } from '../../utils' 
+import { Container, ItemData, Item, Image } from './styles'
 import placeholder from '../../assets/placeholder.jpg'
+import { genres } from '../../api/info'
 
 export const MovieItem = ({movie}) => {
   let url = `/movie/${movie.id}`
   console.log(movie)
+  let curGens = []
+  movie.genre_ids.forEach(id => {
+    genres.forEach(e => {
+      if(id === e.id) {
+        curGens.push(e)
+        return;
+      }    
+    })
+  })
   let imgUrl = !movie.poster_path ? placeholder : `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
-
+  let stars = getStars(movie.vote_average)
   return (
-    <Item>
-      <Link to={url} style={{textDecoration: 'none', color: '#333'}}>
-        <Image alt="example" src={imgUrl} />
-        <br />{movie.original_title}
-      </Link>
-    </Item>
+    <Container>
+      <Item>
+        <Link to={url} style={{textDecoration: 'none', color: '#333'}}>
+          <Image alt="example" src={imgUrl} />
+          <br />{movie.title}
+        </Link>
+      </Item>
+      <ItemData>
+        <h3>Rating:</h3><h3>{stars}</h3>
+        <h3>Release date: </h3><h3>{(movie.release_date)}</h3>
+        <TagList genres={curGens}/>
+      </ItemData>
+    </Container>
   )
 }
+
+// Vote average
+// Release Date
+// Runtime 
+// tags
 
--- src/components/MovieItem/styles.js
+++ src/components/MovieItem/styles.js
@@ -1,16 +1,36 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ItemData = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 200px;
+  justify-content: flex-start;
+  align-items: center;
+  & > * {
+    margin-bottom: 5px;
+    display: none;
+  }
+  ${Container}:hover & > * {
+    display: inherit;
+  }
+`;
+
 export const Item = styled.div`
   max-width: 300px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  text-align: center;
+  justify-content: flex-start;
+  align-items: center;
   font-size: 1.5em;
   margin-bottom: 0.5em;
 `;
 
 export const Image = styled.img`
   max-width: 200px;
-  margin: 0.5em auto;
+  margin-bottom: 0.5em;
 `;
